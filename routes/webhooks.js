8421be1be0400b5fdbafba594f3a715d87f03d03
Jason Cheng <yannru.cheng@gmail.com>
2015-09-08 20:21:24 +0800

    add code to handle norecipes data

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -5,108 +5,143 @@ var Recipe = require('../models/Recipe.js');
 
 /* hook for epicurious api */
 router.post('/epicurious', function(req, res, next) {
+	var isSuccessful = insertDataFromKimonoAPI(req, "Epicurious");
 
-	var results = req.body.results;
-
-	insertDataFromKimonoAPI(results, "Epicurious");
-
-	res.send("successful");
+	if (isSuccessful){
+		res.send("recipe webhook update sucessful");
+	}
+	else{
+		res.send("recipe webhook update canceled");
+	}
 });
 
 router.post('/chow', function(req, res, next) {
+	var isSucessful = insertDataFromKimonoAPI(req, "Chow");
 
-	var results = req.body.results;
-
-	insertDataFromKimonoAPI(results, "Chow");
-
-	res.send("successful");
+	if (isSuccessful){
+		res.send("recipe webhook update sucessful");
+	}
+	else{
+		res.send("recipe webhook update canceled");
+	}
 });
 
-function insertDataFromKimonoAPI(results, defaultPublisher){
-	results.forEach(function(result){
-		// console.log(result);
-		// Run a query to search for the recipe in db first
-		var query = Recipe.where({url:result.url});
-		query.findOne(function(err, recipe){
-			if(err) handleError(err);
+router.post('/norecipes', function(req, res, next){
+	var isSucessful = insertDataFromKimonoAPI(req, "NoRecipes");
 
-			// Insert recipe if not in db
-			if(recipe == null){
-				var recipe = new Recipe; // Create instance from model
-				var basicInfo = result.basicInfo;
+	if (isSuccessful){
+		res.send("recipe webhook update sucessful");
+	}
+	else{
+		res.send("recipe webhook update canceled");
+	}
+});
 
-				// Set the recipe url
-				recipe.basic.url = result.url;
+function insertDataFromKimonoAPI(request, defaultPublisher){
+	//check if data is new
+	var isNewData = request.body.newdata;
+	if (!isNewData){
+		return false;
+	}
+	else {
+		//check for empty results first
+		var results = request.body.results;
 
-				if(basicInfo != null){
+		results.forEach(function(result){
+			// console.log(result);
+			// Run a query to search for the recipe in db first
+			var query = Recipe.where({'basic.url':result.url});
+			query.findOne(function(err, recipe){
+				if(err) handleError(err);
 
-					var info = basicInfo[0];
-					// Photo... skip this recipe if it doesn't have photo
-					var photo = info.photo;
-					if(photo != null)
-						recipe.basic.photo = photo.src;
-					else
-						return; 
+				// Insert recipe if not in db
+				if(recipe == null){
+					var recipe = new Recipe; // Create instance from model
+					var basicInfo = result.basicInfo;
 
-					// Title... skip recipe if it doesn't have a title
-					var title = info.name;
-					if(title != null)
-						recipe.basic.title = title;
-					else
+					// Set the recipe url
+					recipe.basic.url = result.url;
+
+					if(basicInfo != null){
+
+						var info = basicInfo[0];
+						// Photo... skip this recipe if it doesn't have photo
+						var photo = info.photo;
+						if(photo != null)
+							recipe.basic.photo = photo.src;
+						else
+							return; 
+
+						// Title... skip recipe if it doesn't have a title
+						var title = info.name;
+						if(title != null)
+							recipe.basic.title = title;
+						else
+							return;
+
+						// Yield (how many servings)
+						var yield = info.yeild;
+						if(yield != null) 
+							recipe.basic.yield = String(yield);
+
+				  		// Total Time
+				  		var totalTime = info.totalTime;
+				  		if(totalTime != null)
+							recipe.basic.totalTime = String(totalTime);
+
+						// Publisher
+						var publisher = info.publisher;
+						if(publisher != null)
+							recipe.basic.publisher = String(publisher);
+						else
+							recipe.basic.publisher = defaultPublisher;
+
+						// Author
+						var author = info.author;
+						if(author != null)
+							recipe.basic.author = String(author);
+
+					}
+					else{
+						// Skip recipe if basic info doesn't exist... bad data
 						return;
+					}
 
-					// Yield (how many servings)
-					var yield = info.yeild;
-					if(yield != null) 
-						recipe.basic.yield = String(yield);
+					var ingredients = result.ingredients;
+					if(ingredients != null){
+						ingredients.forEach(function(step){
 
-			  		// Total Time
-			  		var totalTime = info.totalTime;
-			  		if(totalTime != null)
-						recipe.basic.totalTime = String(totalTime);
+							var ingredientString = ""
+							if(defaultPublisher == "NoRecipes"){
+								ingredientString = step.quantity + step.unit + step.ingredient;
+							}
+							else {
+								ingredientString = step.ingredient;
+							}
 
-					// Publisher
-					var publisher = info.publisher;
-					if(publisher != null)
-						recipe.basic.publisher = String(publisher);
-					else
-						recipe.basic.publisher = defaultPublisher;
+							recipe.ingredients.push(String(ingredientString));
+						});
+					}
 
-					// Author
-					var author = info.author;
-					if(author != null)
-						recipe.basic.author = String(author);
+					// Directions
+					var directions = result.directions;
+					if(directions != null){
+				  		directions.forEach(function(step){
+				  			recipe.directions.push(String(step.direction));
+				  		});
+					}
 
-				}
-				else{
-					// Skip recipe if basic info doesn't exist... bad data
-					return;
-				}
-
-				var ingredients = result.ingredients;
-				if(ingredients != null){
-					ingredients.forEach(function(step){
-						recipe.ingredients.push(String(step.ingredient));
+					recipe.save(function(err, product, numberAffected){
+						if(err){
+							console.error(err);
+						}
+						
 					});
 				}
-
-				// Directions
-				var directions = result.directions;
-				if(directions != null){
-			  		directions.forEach(function(step){
-			  			recipe.directions.push(String(step.direction));
-			  		});
-				}
-
-				recipe.save(function(err, product, numberAffected){
-					if(err){
-						console.error(err);
-					}
-					
-				});
-			}
+			});
 		});
-	});
+	}
+	return true;
 }
 
 module.exports = router;
